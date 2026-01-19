6d732c3459dd159382afe787fe30978dd7eee795
Anday Turgut <at.andayturgut@gmail.com>
2026-01-19 22:28:54 +0300

    Enhance Audio.h with function comments
    Added comments to clarify the purpose of functions in Audio.h.

--- include/Audio.h
+++ include/Audio.h
@@ -9,10 +9,11 @@ public:
     static void Close();
     static void Update();
 
+    // It loads sound effects and music into memory by naming them.
     static void LoadSFX(std::string name, const char* path);
     static void LoadMusic(std::string name, const char* path);
 
-    
+    // Plays a sound effect. By changing the pitch, the same sound can be produced in different tones.
     static void PlaySFX(std::string name, float volume = 1.0f, float pitch = 1.0f);
 
     static void PlayMusic(std::string name);
@@ -23,5 +24,7 @@ public:
 private:
     static std::map<std::string, Sound> sounds;
     static std::map<std::string, Music> musics;
+
+    // Holds the currently playing music. It's necessary to know which music to update in the Update() function.
     static Music* currentMusic;
 };
